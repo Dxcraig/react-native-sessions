a4a54c1212a6f6bcc492222434634f057c35d31d
Dxcraig <steviedoe20@gmail.com>
2025-09-27 20:08:49 +0000

    Update App.js to display a personalized greeting message with styling

--- MyApp/App.js
+++ MyApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.helloText}>Hello, Stephen Doe-lawson!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  helloText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
